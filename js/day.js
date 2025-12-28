a7e79cf6ca7d8a4c71b6844523814e79204b58ce
Raphaelle <raphaelle.meuleman@etudiant.univ-reims.fr>
2025-12-28 12:12:16 +0100

    day.js mise à jour pour qu'il y ai que les erreurs qui apparaissent et pas les éléments du jour

--- js/day.js
+++ js/day.js
@@ -3,14 +3,22 @@ function checkDate(day) {
   const currentDay = today.getDate();
   const currentMonth = today.getMonth(); // janvier = 0
 
-  const content = document.getElementById("content");
+  const content = document.getElementById("day-content");
 
   // if (currentMonth !== 0 || currentDay !== day) {
-    if (false) {  // ignore la date pour tester
-    content.innerHTML = `
+  if (false) {  // ignore la date pour tester
+    // On supprime le contenu du jour
+    content.style.display = "none";
+
+    // On crée le message de verrouillage
+    const locked = document.createElement("div");
+    locked.className = "home-container";
+    locked.innerHTML = `
       <h1>⛔ Oups…</h1>
       <p>Cette surprise n’est pas encore disponible.</p>
       <p>Reviens le bon jour 💕</p>
     `;
+
+    document.body.appendChild(locked);
   }
 }
